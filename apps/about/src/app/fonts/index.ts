bd28cffe14e36a4228840a800c5c6d6571238916
himchan94 <himchan9494@gmail.com>
2024-11-04 00:46:13 +0900

    feat: apply fonts

---
+++ apps/about/src/app/fonts/index.ts
@@ -0,0 +1,66 @@
+import localFont from "next/font/local";
+
+export const BigShouldersDisplay = localFont({
+  src: [
+    {
+      path: "./bigShouldersDisplay/BigShouldersDisplay-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./bigShouldersDisplay/BigShouldersDisplay-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./bigShouldersDisplay/BigShouldersDisplay-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./bigShouldersDisplay/BigShouldersDisplay-Bold.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
+
+export const JetBrainsMono = localFont({
+  src: [
+    {
+      path: "./jetBrainsMono/JetBrainsMono-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./jetBrainsMono/JetBrainsMono-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./jetBrainsMono/JetBrainsMono-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./jetBrainsMono/JetBrainsMono-Bold.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
+
+export const D2 = localFont({
+  src: [
+    {
+      path: "./d2/D2CodingLigatureNerdFont-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./d2/D2CodingLigatureNerdFont-Bold.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
