49eca6ad5a7fee6a9da85ebed8f34963f73490c9
himchan94 <himchan9494@gmail.com>
2024-11-04 01:37:45 +0900

    feat: apply noise background

--- apps/about/src/app/layout.tsx
+++ apps/about/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "@/styles/global.scss";
 import { BigShouldersDisplay, JetBrainsMono, D2 } from "@/app/fonts";
-
+import Image from "next/image";
+import styles from "./layout.module.scss";
 export const metadata: Metadata = {
   title: "About Developer Himchan",
   description: "프론트엔드 개발자 김힘찬입니다.",
@@ -16,6 +17,11 @@ export default function RootLayout({
     <html lang="ko">
       <body className={`${BigShouldersDisplay} ${JetBrainsMono} ${D2}`}>
         {children}
+        <div className={styles["background-container"]}>
+          <div className={styles["background-content-container"]}>
+            <img className={styles["background-image"]} />
+          </div>
+        </div>
       </body>
     </html>
   );
